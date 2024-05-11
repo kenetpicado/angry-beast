c560639d025cef9b6f9f1779a44972d3d91499e7
Kenet Picado <kenetpicado1@gmail.com>
2024-05-11 15:55:59 -0600

    remove controllers

--- routes/web.php
+++ routes/web.php
@@ -5,42 +5,33 @@ use App\Http\Controllers\Dashboard\CompanyController;
 use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\Dashboard\UserController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
+Route::redirect('/', '/dashboard');
+
+Route::group([
+    'middleware' => ['auth', 'last.active'],
+    'prefix' => 'dashboard',
+    'as' => 'dashboard.'],
+    function () {
+        Route::get('/', DashboardController::class)
+            ->name('index');
+
+        Route::resource('companies', CompanyController::class)
+            ->only(['index', 'update', 'store']);
+
+        Route::resource('clients', ClientController::class)
+            ->except(['edit', 'create', 'show']);
+
+        Route::resource('users', UserController::class)
+            ->except(['edit', 'create', 'show']);
+    });
+
+Route::middleware(['auth', 'last.active'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::put('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
 });
-
-Route::group(['middleware' => ['auth', 'last.active'], 'prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
-    Route::get('/', DashboardController::class)
-        ->name('index');
-
-    Route::resource('companies', CompanyController::class)
-        ->only(['index', 'update', 'store']);
-
-    Route::resource('clients', ClientController::class)
-        ->except(['edit', 'create', 'show']);
-
-    Route::resource('users', UserController::class)
-        ->except(['edit', 'create', 'show']);
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
-});
-
-require __DIR__.'/auth.php';
